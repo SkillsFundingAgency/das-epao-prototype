6bbeb0f35154b15bdb3ea01189a877eb63f9e234
hazelfletcher <hazel.fletcher@digital.education.gov.uk>
2021-08-24 12:21:57 +0100

    routes.js file for select certificate receiver

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// record-a-grade: address
+router.post('/record-a-grade/send-the-certificate', (req, res) => {
+	if(req.session.data['send-the-certificate'] == 'apprentice'){
+		res.redirect('apprentice-search-address')
+	} else if(req.session.data['send-the-certificate'] == 'employer'){
+        res.redirect('employer-address')
+	} else if(req.session.data['send-the-certificate'] == 'provider'){
+        res.redirect('provider-address')
+    }
+})
+
 module.exports = router
