62d37d133c49a1a3f0a86526c322dd1802770d95
hazelfletcher <hazel.fletcher@digital.education.gov.uk>
2021-09-13 10:57:47 +0100

    sub folders created for v1 - address saved and no address pre saved employer journeys

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,18 @@ router.post('/record-a-grade/send-the-certificate', (req, res) => {
     }
 });
 
+// record-a-grade: who to send certificate to
+router.post('/record-a-grade/v1/no-address-saved/send-the-certificate', (req, res) => {
+	if(req.session.data['send-the-certificate'] == 'apprentice'){
+		res.redirect('apprentice-search-address');
+	} else if(req.session.data['send-the-certificate'] == 'employer'){
+        res.redirect('employer-address');
+	} else if(req.session.data['send-the-certificate'] == 'provider'){
+        res.redirect('provider-address');
+    }
+});
+
+
 // record-a-grade: apprentice search address
 router.post('/record-a-grade/apprentice-find-address', function (req, res) {
 	if(req.session.data['street-number-house-name'] == '14'){
@@ -25,8 +37,19 @@ router.post('/record-a-grade/apprentice-find-address', function (req, res) {
     } else res.redirect('apprentice-select-address');
 });
 
-// record-a-grade: select employer address
-router.post('/record-a-grade/employer-address-saved', function (req, res) {
+// record-a-grade: employer search address
+router.post('/record-a-grade/v1/address-saved/employer-find-address', function (req, res) {
+	if(req.session.data['company-building-name'] == '14'){
+		res.redirect('employer-select-address-1');
+	} else if(req.session.data['company-building-name'] == 'test'){
+        res.redirect('employer-select-address-0');
+	} else if(req.session.data['company-building-name'] == 'Test'){
+        res.redirect('apprentice-select-address-0');
+    } else res.redirect('employer-select-address');
+});
+
+// record-a-grade: select employer address (saved address)
+router.post('/record-a-grade/v1/address-saved/employer-address-saved', function (req, res) {
 	if(req.session.data['choose-employer-address'] == 'address-system'){
 		res.redirect('employer-details');
 	} else if(req.session.data['choose-employer-address'] == 'address-last-used'){
@@ -36,16 +59,7 @@ router.post('/record-a-grade/employer-address-saved', function (req, res) {
     } 
 });
 
-// record-a-grade: employer search address
-router.post('/record-a-grade/employer-find-address', function (req, res) {
-	if(req.session.data['company-building-name'] == '14'){
-		res.redirect('employer-select-address-1');
-	} else if(req.session.data['company-building-name'] == 'test'){
-        res.redirect('employer-select-address-0');
-	} else if(req.session.data['company-building-name'] == 'Test'){
-        res.redirect('apprentice-select-address-0');
-    } else res.redirect('employer-select-address');
-});
+
 
 
 
